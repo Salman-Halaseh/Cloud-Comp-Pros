a10ed0b7f216d905fc77181cd75057870b45b917
Salman Halaseh <48489172+Salman-Halaseh@users.noreply.github.com>
2026-01-26 18:53:21 +0300

    Mac

---
+++ config.js
@@ -0,0 +1,9 @@
+// Firebase configuration - DO NOT COMMIT THIS FILE
+const firebaseConfig = {
+    apiKey: "AIzaSyDyQxk_fKv-2NUwM4ZBmky4fpMe7MIb62w",
+    authDomain: "cloud-comp-pro.firebaseapp.com",
+    projectId: "cloud-comp-pro",
+    storageBucket: "cloud-comp-pro.firebasestorage.app",
+    messagingSenderId: "930499187231",
+    appId: "1:930499187231:web:51636dc1ab7eeb7bd5bb2a"
+};
